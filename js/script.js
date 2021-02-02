84f0c3a4bd2d99a31a9575523766bc596364b777
kiJu2 <thdydehfdl12@gmail.com>
2021-02-02 14:08:52 +0900

    Init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload = () =>{
+	var typed = new Typed('.typed', {
+		strings: ['원하는 글씨를'],
+		typeSpeed: 100,
+		// backSpeed: 100,
+		fadeOut: false,
+		// smartBackspace: true,
+		cursorChar: '|',
+		// loop: true
+	});
+}
